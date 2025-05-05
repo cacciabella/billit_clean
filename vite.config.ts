269b6623c2fe0dc52e464b8a1041a2fc6a6f08c4
cacciabella12 <cacciabella12@gmail.com>
2025-05-05 23:15:57 +0200

    fix: cast PORT to number for TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,11 @@ dotenv.config({ path: './frontend.env' });
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Rende la porta accessibile dall'esterno
+    port: Number(process.env.PORT) || 3000,
+    // Usa la porta fornita da Render, altrimenti 3000
     proxy: {
       '/invoices': 'http://localhost:8080',
     },
-   
   },
-})
+});
